c55299a31c9b6b238b1f684c415c7f4138246ced
Jonathan Speiser <jspeis@gmail.com>
2017-01-17 10:35:28 -0500

    refactoring crops scripts. adding water supply breakdown for production value

--- data_africa/plugins/cell5m.py
+++ data_africa/plugins/cell5m.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 def crop_format(df, **kwargs):
     index = kwargs.get("index")
-    df = pd.melt(df, id_vars=index, var_name="crop", value_name="harvested_area")
-    is_rain_fed = df.crop.str.endswith("_r_h")
-    is_irr = df.crop.str.endswith("_i_h")
+    mode = kwargs.get("mode", "h")
+    vname = "harvested_area" if mode == "h" else "value_of_production"
+    df = pd.melt(df, id_vars=index, var_name="crop", value_name=vname)
+    is_rain_fed = df.crop.str.endswith("_r_{}".format(mode))
+    is_irr = df.crop.str.endswith("_i_{}".format(mode))
     df.loc[is_rain_fed, 'water_supply'] = 'rainfed'
     df.loc[is_irr, 'water_supply'] = 'irrigated'
     df.loc[(~is_irr & ~is_rain_fed), 'water_supply'] = 'overall'
-    df.crop = df.crop.str.replace("(_r_h|_i_h|_h)", "")
+    df.crop = df.crop.str.replace("(_r_{0}|_i_{0}|_{0})".format(mode), "")
     return df
